aba8510fc8f2ba630cb50dc2c5607afbfeaa0b04
newstar94 <77623864+newstar94@users.noreply.github.com>
2021-07-12 17:13:00 +0700

    Cookingrecipe

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ owl.on('mousewheel', '.owl-stage', function (e) {
     }
     e.preventDefault();
 });
+function loadmore(){
+    var p=[];
+    $.ajax({
+        url:"data/recipes.json",
+        method: "GET",
+        success: function (rs){
+            p=rs;
+            var covid = $("#covid");
+            for (var i=0;i<p.length;i++){
+                var ct = "<div class=\"col-md-3\">\n" +
+                    "                        <div class=\"recipes\">\n" +
+                    "                            <a href=\"#\">\n" +
+                    "                                <img class=\"cover\" src=\""+p[i].image+"\">\n" +
+                    "                                <h3>"+p[i].name+"</h3>\n" +
+                    "                                <p>"+p[i].description+"</p>\n" +
+                    "                            </a>\n" +
+                    "                        </div>";
+                covid.append(ct);
+            }
+        }
+    });
+}
